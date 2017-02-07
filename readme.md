d81b50b5f9cfe786d606a60b9c5e0e0bf5aa554f
Julio Blanco <julioblancorangel@hotmail.com>
2017-02-07 11:58:01 -0400

    Base de App

---
+++ readme.md
@@ -0,0 +1,6 @@
+## Simple App for Zoho Invoices (Challenge Alegra) / Aplicación Sencilla para Invoices de Zoho (Reto Alegra)
+
+- git clone
+- cd zoho-laravel-angular
+- composer update
+- php artisan key:generate
---
+++ resources/views/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Zoho Laravel-Angular Invoices</title>
+        <!--Bootstrap-->
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!--Angular-->
+        <script src="bower_components/angular/angular.min.js"></script>
+        <script src="bower_components/lodash/lodash.min.js"></script>
+        <script src="bower_components/angular-route/angular-route.min.js"></script>
+        <script src="bower_components/angular-local-storage/dist/angular-local-storage.min.js"></script>
+        <script src="bower_components/restangular/dist/restangular.min.js"></script>
+        <!--Angular-->
+
+        <style>
+            
+            li {
+                padding-bottom: 8px;
+            }
+
+        </style>
+    </head>
+    <body>
+        <!--Contenedor Principal-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Aplicacion de Invoices de Zoho</h1>
+                </div>
+            </div>
+        </div>
+        <!--Contenedor Principal-->
+
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
